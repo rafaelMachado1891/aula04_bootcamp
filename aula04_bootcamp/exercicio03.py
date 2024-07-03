3e54111c13ba01fce87ae64950133cf1b4a8a692
rafaelMachado1891 <rafadmn1@gmail.com>
2024-07-02 22:24:09 -0300

    resolucao exercicio03

---
+++ aula04_bootcamp/exercicio03.py
@@ -0,0 +1,20 @@
+# Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. Imprima cada informação.
+
+Livro: dict = {
+    "titulo": "Pai Pai Pobre",
+    "autor": "Robert T. Kiyosaki",
+    "publicacao": 2017
+}
+
+livro2: dict = {
+    "titulo": "Storytelling com dados",
+    "autor": "cole nussbaumer knaflic",
+    "publicacao": 2019
+}
+
+lista: list = []
+
+lista.append(Livro)
+lista.append(livro2)
+
+print(lista)
