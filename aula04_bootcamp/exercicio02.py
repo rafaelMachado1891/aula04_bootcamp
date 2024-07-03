2bf7d45364a33d1033cb3577529684404f90aabe
rafaelMachado1891 <rafadmn1@gmail.com>
2024-07-02 22:03:01 -0300

    resolucao exercicio02.py

---
+++ aula04_bootcamp/exercicio02.py
@@ -0,0 +1,7 @@
+# Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
+
+lista: list = ["Python", "Java", "C++", "JavaScript"]
+lista.remove("C++")
+lista.append("Ruby")
+
+print(lista)
