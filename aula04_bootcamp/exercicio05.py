9c93a3f5acaff80e78f7e8e7ee09ca37906816af
rafaelMachado1891 <rafadmn1@gmail.com>
2024-07-07 08:47:09 -0300

    resolucao exercicio05

--- aula04_bootcamp/exercicio05.py
+++ aula04_bootcamp/exercicio05.py
@@ -1,7 +1,11 @@
-def contar_caracteres(s):
-    contagem = {}
-    for caractere in s:
-        contagem[caractere] = contagem.get(caractere, 0) + 1
-    return contagem
+# Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}, 
+# calcule o preço total da lista de compras.
 
-print(contar_caracteres("engenharia de dados"))
+lista: list = ['maçã', 'banana', 'cereja']
+dicionario: dict = {"maçã": 0.45, "banana": 0.30, "cereja": 0.65, "picanha": 100.00}
+
+
+
+resultado = sum(dicionario[valores] for valores in lista)
+
+print(f"O total das suas compras é: {resultado}")
